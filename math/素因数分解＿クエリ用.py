2f004742dbd6b000e91288f079d367d1b5b25395
yutomatsu <102792097+yutomatsu@users.noreply.github.com>
2023-05-11 10:04:33 +0900

    Add files via upload

---
+++ math/素因数分解＿クエリ用.py
@@ -0,0 +1,59 @@
+def smallest_prime_factors(n):
+    """各数の最小素因数を求める"""
+    spf = [i for i in range(n+1)]
+    for i in range(2,n+1):
+        if i*i>n:
+            break
+        if spf[i] == i:
+            for j in range(i*i, n+1, i):
+                if spf[j] == j:
+                    spf[j] = i
+    return spf
+
+def fac(x, spf):
+    """xを素因数分解,素因数のみ欲しい場合はsetに"""
+    ret = []
+    while x!=1:
+        ret.append(spf[x])
+        x //= spf[x]
+    ret.sort()
+    return ret
+
+def fac2(x, spf) -> dict:
+    """辞書で返す"""
+    ret = {}
+    while x!=1:
+        if spf[x] in ret:
+            ret[spf[x]] += 1
+        else:
+            ret[spf[x]] = 1
+        x //= spf[x]
+    return ret
+
+def divisor(x,spf):
+    """xの約数を列挙して返す"""
+    ret = fac2(x,spf)
+    div = [1]
+    for key in ret.keys():
+        s = len(div)
+        for i in range(s):
+            nn = key
+            for j in range(1,ret[key]+1):
+                div.append(div[i]*nn)
+                nn *= key
+    return div
+
+def mobius(x,spf):
+    """
+    メビウス関数の結果返す。
+    μ(1) = 1
+    ある素数で2回以上割り切れる場合はμ(x)=0
+    全ての素因数が1回までしか出現しない
+    μ(x)=(-1)^K Kは素因数の数
+    """
+
+spf = smallest_prime_factors(10000)
+print(divisor(120,spf))
+A = [10, 4, 20]
+for i in range(3):
+    print(fac2(A[i], spf))
